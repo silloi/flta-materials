0386d556379d3e39375baf1dc71aedbf10fb8ee0
silloi <silloi1993@gmail.com>
2021-12-29 20:00:26 +0900

    feature: complete 03

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
   // 1
@@ -10,19 +12,12 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
-    // TODO: Apply Home widget
-    // 3
+    final theme = FooderlichTheme.dark();
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
       // 4
-      home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
-      ),
+      home: const Home(),
     );
   }
 }
