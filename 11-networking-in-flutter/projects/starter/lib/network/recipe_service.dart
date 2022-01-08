17d9e8335601a3dbccaf3030bbe8c43f7f6d5dc8
silloi <silloi1993@gmail.com>
2022-01-08 12:29:26 +0900

    feature: complete 11

---
+++ 11-networking-in-flutter/projects/starter/lib/network/recipe_service.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart';
+
+const String apiKey = 'a1c08d5426bd397fa5afda11f3d0eb7e';
+const String apiId = '2ba88aad';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  Future getData(String url) async {
+    print('Calling url: $url');
+    final response = await get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to'
+    );
+    return recipeData;
+  }
+}
